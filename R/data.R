d694cb58b36fc3ade9e6b6f27d1e29bcda7c34e7
vijaykumarmuley <vijaykumar.muley@outlook.de>
2021-07-23 20:25:27 -0500

    Fix such and such

--- R/data.R
+++ R/data.R
@@ -5,7 +5,7 @@
 #'
 #' @docType data
 #'
-#' @usage data(pvalue)
+#' @usage data(pvaluevym)
 #'
 #' @format A data frame with 11678 rows and 8 variables:
 #' \describe{
@@ -20,4 +20,4 @@
 #' }
 #'
 #' @source \url{https://doi.org/10.1016/j.pneurobio.2019.101735}
-"pvalue"
+"pvaluevym"
